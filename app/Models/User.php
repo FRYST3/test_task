cd615d61dafff4eab3e8243fa7c2d45550a41217
Gleb Fryst <bloodgrab317@yandex.by>
2022-12-11 19:00:11 +0300

    Получаем из бд таблицу users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'id','username', 'password', 'balance'
+    ];
 }
