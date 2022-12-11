9202970cfeda5e242d7b8ea000da5f38c0425c20
Gleb Fryst <bloodgrab317@yandex.by>
2022-12-11 18:58:31 +0300

    Настройка routing and Controllers

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\Datatables;
 
 class PagesController extends Controller
 {
-    //
+    public function index() {
+        return view('index');
+    }
 }
